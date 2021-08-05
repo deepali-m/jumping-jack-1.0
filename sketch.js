44d44d3f0bc58cdaf8d6e5d942dc6e8e0b221f2c
deepali-m <dmokashi@asu.edu>
2021-08-05 18:27:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+
+var player,player2;
+var platform,platform2;
+var player_image, platform_image;
+
+function preload(){
+  player_image=loadImage("images/jack2.png")
+  platform_image = loadImage("images/p1.png");
+  player2_image=loadImage("images/jack2.png")
+  platform2_image = loadImage("images/p2.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  player = createSprite(250,300,20,50);
+  player.addImage("player",player_image)
+
+  platform = createSprite(250,350,100,10);
+  platform.addImage("platform", platform_image)
+  platform.scale=0.15
+
+  
+  
+}
+
+function draw() {
+  background('#F2F5B6');
+  
+  if(keyDown("up"))
+  {
+    player.velocityY=-10
+  }
+  
+  drawSprites();
+}
+
+
+
+
+
+
+
+
+
+
